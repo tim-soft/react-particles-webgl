0e66b48f40f453cf5b6bcc2c6f12ef8acab388e2
Tim Ellenberger <timellenberger@gmail.com>
2019-03-26 11:14:15 -0700

    Add comments to animate function

--- src/lib/animate.js
+++ src/lib/animate.js
@@ -4,9 +4,9 @@
 /* eslint-disable no-param-reassign */
 
 /**
- * Animates an array of particles over a three dimensional space
+ * Animates an array of particles and lines over a three dimensional space
  *
- * Calculates the positions of lines between particles
+ * This function is meant to be called from the useRender render loop -- ran on each frame
  */
 const animate = ({
   minDistance,
@@ -21,19 +21,27 @@ const animate = ({
   lineColors,
   bounds
 }) => {
+  // Get the boundary points of the canvas
+  // Useful for knowing when a particle is at the edge of the canvas
   const { xBounds, yBounds, zBounds } = bounds;
 
   let vertexpos = 0;
   let colorpos = 0;
   let numConnected = 0;
+
+  // Start by assuming no line connections between particles
   for (let i = 0; i < particleCount; i += 1)
     particlesData[i].numConnections = 0;
+
+  // Update the [x, y, z] position of each particle
   for (let i = 0; i < particleCount; i += 1) {
-    // get the particle
+    // The current particle
     const particleData = particlesData[i];
     particlePositions[i * 3] += particleData.velocity.x;
     particlePositions[i * 3 + 1] += particleData.velocity.y;
     particlePositions[i * 3 + 2] += particleData.velocity.z;
+
+    // Make the current particle "bounce" off of the "bounds" of the canvas
     if (
       particlePositions[i * 3 + 1] < -yBounds / 2 ||
       particlePositions[i * 3 + 1] > yBounds / 2
@@ -49,21 +57,38 @@ const animate = ({
       particlePositions[i * 3 + 2] > zBounds / 2
     )
       particleData.velocity.z = -particleData.velocity.z;
+
+    // Skip to next particle if we are intentionally no drawing lines
     if (limitConnections && particleData.numConnections >= maxConnections)
       continue;
-    // Check collision
+
+    // Calculate the distance between particles to find nearest-neighbors
+    // If particles fall within the threshold, draw a line connecting them
     for (let j = i + 1; j < particleCount; j += 1) {
+      // The "other" particle to compare to "this" one
       const particleDataB = particlesData[j];
+
+      // Skip to next particle(don't draw more lines) if some limiting factor has been met
       if (limitConnections && particleDataB.numConnections >= maxConnections)
         continue;
+
+      // Calculate the distance between "this" particle and the "other" particle
       const dx = particlePositions[i * 3] - particlePositions[j * 3];
       const dy = particlePositions[i * 3 + 1] - particlePositions[j * 3 + 1];
       const dz = particlePositions[i * 3 + 2] - particlePositions[j * 3 + 2];
       const dist = Math.sqrt(dx * dx + dy * dy + dz * dz);
+
+      // If "this" particle is close enough to the "other" particle, draw a line connecting the two
       if (dist < minDistance) {
+        // Increment the number of connections by one on both particles
         particleData.numConnections += 1;
         particleDataB.numConnections += 1;
+
+        // The lines become more transparent the further apart the particles become
+        // The line fragment shader applies this alpha attribute when coloring the lines
         const alpha = 1.0 - dist / minDistance;
+
+        // Set the line position's [x, y, z] position at "this" point and the "other" point
         linePositions[vertexpos++] = particlePositions[i * 3];
         linePositions[vertexpos++] = particlePositions[i * 3 + 1];
         linePositions[vertexpos++] = particlePositions[i * 3 + 2];
@@ -81,6 +106,7 @@ const animate = ({
     }
   }
 
+  // Signal to Three.js that the line and point geometry have changed
   lineMeshGeometry.setDrawRange(0, numConnected * 2);
   lineMeshGeometry.attributes.position.needsUpdate = true;
   lineMeshGeometry.attributes.color.needsUpdate = true;
