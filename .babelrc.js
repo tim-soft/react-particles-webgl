1b24262cbd4e3761e5b9aaab988621e28b19eedc
Tim Ellenberger <timellenberger@gmail.com>
2019-03-29 19:46:45 -0700

    Cleanup Rollup and babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,15 +1,21 @@
 module.exports = {
   presets: [
-    [
-      '@babel/env',
-      {
-        targets: {
-          browsers: 'last 2 Chrome versions',
-          node: 'current'
-        }
-      }
-    ],
+    // [
+    //   '@babel/env'
+    //   {
+    //     targets: {
+    //       browsers: 'last 2 Chrome versions',
+    //       node: 'current'
+    //     }
+    //   }
+    // ],
+    '@babel/env',
     '@babel/react'
   ],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    ['@babel/plugin-proposal-class-properties'],
+    ['@babel/plugin-proposal-object-rest-spread'],
+    ['transform-react-remove-prop-types', { removeImport: true }],
+    ['@babel/plugin-transform-runtime', { regenerator: false }]
+  ]
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,68 +1,34 @@
 import path from 'path';
 import babel from 'rollup-plugin-babel';
+import commonjs from 'rollup-plugin-commonjs';
+import filesize from 'rollup-plugin-filesize';
 import resolve from 'rollup-plugin-node-resolve';
-// import commonjs from 'rollup-plugin-commonjs';
-// import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 
 import pkg from './package.json';
 
 const root = process.platform === 'win32' ? path.resolve('/') : '/';
-const external = id => !id.startsWith('.') && !id.startsWith(root);
-const extensions = ['.js', '.jsx', '.ts', '.tsx'];
-const getBabelOptions = ({ useESModules }, targets) => ({
-  babelrc: false,
-  extensions,
-  exclude: '**/node_modules/**',
-  runtimeHelpers: true,
-  presets: [
-    ['@babel/preset-env', { loose: true, modules: false, targets }],
-    '@babel/preset-react',
-    '@babel/preset-typescript'
-  ],
-  plugins: [
-    ['@babel/plugin-proposal-class-properties', { loose: true }],
-    ['@babel/plugin-proposal-object-rest-spread', { loose: true }],
-    ['transform-react-remove-prop-types', { removeImport: true }],
-    ['@babel/plugin-transform-runtime', { regenerator: false, useESModules }]
-  ]
-});
 
-function createConfig() {
-  return [
+export default {
+  input: 'src/index.js',
+  output: [
     {
-      input: 'src/index.js',
-      output: {
-        file: pkg.module,
-        format: 'es',
-        sourcemap: true
-      },
-      external,
-      plugins: [
-        babel(
-          getBabelOptions(
-            { useESModules: true },
-            '>1%, not dead, not ie 11, not op_mini all'
-          )
-        ),
-        // sizeSnapshot(),
-        resolve({ extensions })
-      ]
+      file: pkg.main,
+      format: 'cjs',
+      sourcemap: true
     },
     {
-      input: 'src/index.js',
-      output: {
-        file: pkg.main,
-        format: 'cjs',
-        sourcemap: true
-      },
-      external,
-      plugins: [
-        babel(getBabelOptions({ useESModules: false })),
-        // sizeSnapshot(),
-        resolve({ extensions })
-      ]
+      file: pkg.module,
+      format: 'es',
+      sourcemap: true
     }
-  ];
-}
-
-export default [...createConfig()];
+  ],
+  external: id => !id.startsWith('.') && !id.startsWith(root),
+  plugins: [
+    babel({
+      runtimeHelpers: true
+    }),
+    resolve(),
+    commonjs(),
+    filesize()
+  ]
+};
