0c0b8cafde21928cd2c3b360be1ad0228c2f83b4
Tim Ellenberger <timellenberger@gmail.com>
2019-03-29 19:48:22 -0700

    Remove comment from babel config

--- .babelrc.js
+++ .babelrc.js
@@ -1,17 +1,5 @@
 module.exports = {
-  presets: [
-    // [
-    //   '@babel/env'
-    //   {
-    //     targets: {
-    //       browsers: 'last 2 Chrome versions',
-    //       node: 'current'
-    //     }
-    //   }
-    // ],
-    '@babel/env',
-    '@babel/react'
-  ],
+  presets: ['@babel/env', '@babel/react'],
   plugins: [
     ['@babel/plugin-proposal-class-properties'],
     ['@babel/plugin-proposal-object-rest-spread'],
