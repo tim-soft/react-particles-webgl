31cfeb612ad439484a1749f7590817001dba0d3d
Tim Ellenberger <timellenberger@gmail.com>
2019-08-26 22:14:16 -0700

    Gracefully handle server side rendering

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Canvas } from 'react-three-fiber';
 import merge from 'lodash.merge';
@@ -6,19 +6,34 @@ import ParticleField from './ParticleField';
 import initialConfig from './config';
 
 /**
- * Wraps a particle field in a Canvas
+ * Creates a 2D/3D particle field with react-three-fiber, three.js and WebGL
+ *
+ * Documentation on the configuration object can be found in the github repo
+ * @see https://github.com/tim-soft/react-particles-webgl
+ *
+ * For a real-time configuration generator and various demos
+ * @see https://timellenberger.com/particles
  */
-const ParticleCanvas = ({ config }) => (
-  <Canvas
-    gl={{
-      antialias: Object.prototype.hasOwnProperty.call(config, 'antialias')
-        ? config.antialias
-        : initialConfig.antialias
-    }}
-  >
-    <ParticleField {...merge({}, initialConfig, config)} />
-  </Canvas>
-);
+const ParticleCanvas = ({ config }) => {
+  const [clientSide, setClientSide] = useState(false);
+  useEffect(() => {
+    setClientSide(true);
+  }, []);
+
+  if (!clientSide) return null;
+
+  return (
+    <Canvas
+      gl={{
+        antialias: Object.prototype.hasOwnProperty.call(config, 'antialias')
+          ? config.antialias
+          : initialConfig.antialias
+      }}
+    >
+      <ParticleField {...merge({}, initialConfig, config)} />
+    </Canvas>
+  );
+};
 
 ParticleCanvas.propTypes = {
   config: PropTypes.object
